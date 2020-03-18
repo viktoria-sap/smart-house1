f058c3291d7b71e04c1a8c1902cc2d3b5631a4fb
Viktoria <v.sapunkova@neurobotics.ru>
2020-03-18 17:43:15 +0300

    initial commit

--- src/main/java/ru/netology/Radio.java
+++ src/main/java/ru/netology/Radio.java
@@ -10,14 +10,6 @@ public class Radio {
     boolean next;
     boolean prev;
 
-    private int maxVolume;
-    private int minVolume;
-    private int currentVolume;
-    private int increaseCurrentVolume;
-    private int decreaseCurrentVolume;
-    boolean increase;
-    boolean decrease;
-
     public boolean isNext(boolean b) {
         return next;
     }
@@ -34,36 +26,27 @@ public class Radio {
         this.prev = prev;
     }
 
-
-    public int getMaxStation () {
-        return maxStation;
-    }
-
-    public void setMaxStation ( int maxStation){
+    public void setMaxStation(int maxStation) {
         this.maxStation = maxStation;
     }
 
-    public int getMinStation () {
-        return minStation;
-    }
-
-    public void setMinStation ( int minStation){
+    public void setMinStation(int minStation) {
         this.minStation = minStation;
     }
 
-    public int getNumberCurrentStation () {
+    public int getNumberCurrentStation() {
         return numberCurrentStation;
     }
 
-    public void setNumberCurrentStation ( int numberCurrentStation){
+    public void setNumberCurrentStation(int numberCurrentStation) {
         this.numberCurrentStation = numberCurrentStation;
     }
 
-    public int getNextStation () {
+    public int getNextStation() {
         return nextStation;
     }
 
-    public void setNextStation (int nextStation){
+    public void setNextStation(int nextStation) {
         if (isPrev(true)) {
             return;
         }
@@ -74,11 +57,11 @@ public class Radio {
         this.nextStation = numberCurrentStation;
     }
 
-    public  int getPrevStation () {
+    public int getPrevStation() {
         return prevStation;
     }
 
-    public void setPrevStation (int prevStation){
+    public void setPrevStation(int prevStation) {
         if (isNext(true)) {
             return;
         }
@@ -89,73 +72,4 @@ public class Radio {
         this.prevStation = numberCurrentStation;
     }
 
-    public boolean isIncrease(boolean b) {
-        return increase;
-    }
-
-    public void setIncrease(boolean increase) {
-        this.increase = increase;
-    }
-
-    public boolean isDecrease(boolean b) {
-        return decrease;
-    }
-
-    public void setDecrease(boolean decrease) {
-        this.decrease = decrease;
-    }
-
-    public int getMaxVolume () {
-        return maxVolume;
-    }
-
-    public void setMaxVolume ( int maxVolume){
-        this.maxVolume = maxVolume;
-    }
-
-    public int getMinVolume () {
-        return minVolume;
-    }
-
-    public void setMinVolume ( int minVolume){
-        this.minVolume = minVolume;
-    }
-
-    public int getCurrentVolume () {
-        return currentVolume;
-    }
-
-    public void setCurrentVolume ( int currentVolume){
-        this.currentVolume = currentVolume;
-    }
-
-    public int getIncreaseCurrentVolume () {
-        return increaseCurrentVolume;
-    }
-
-    public void setIncreaseCurrentVolume ( int increaseCurrentVolume){
-        if (isDecrease(true)) {
-            return;
-        }
-        currentVolume += 1;
-        if (currentVolume > maxVolume) {
-            currentVolume = maxVolume;
-        }
-        this.increaseCurrentVolume = currentVolume;
-    }
-
-    public int getDecreaseCurrentVolume () {
-        return decreaseCurrentVolume;
-    }
-
-    public void setDecreaseCurrentVolume ( int decreaseCurrentVolume){
-        if (isIncrease(true)) {
-            return;
-        }
-        currentVolume -= 1;
-        if (currentVolume < minVolume) {
-            currentVolume = minVolume;
-        }
-        this.increaseCurrentVolume = currentVolume;
-    }
 }
--- src/test/java/ru/netology/RadioTest.java
+++ src/test/java/ru/netology/RadioTest.java
@@ -4,7 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class RadioTest {
 
     @Test
-    public void shouldNextStation(){
+    public void shouldNextStation() {
         Radio radio = new Radio();
 
         radio.setMaxStation(9);
@@ -14,15 +14,15 @@ class RadioTest {
         radio.setNext(true);
         radio.setPrev(false);
 
-        assertEquals( true, radio.isNext(true));
-        assertEquals( false, radio.isPrev(false));
-        assertEquals( 6, radio.getNumberCurrentStation());
+        assertEquals(true, radio.isNext(true));
+        assertEquals(false, radio.isPrev(false));
+        assertEquals(6, radio.getNumberCurrentStation());
         radio.setNextStation(7);
-        assertEquals( 7, radio.getNextStation());
+        assertEquals(7, radio.getNextStation());
     }
 
     @Test
-    public void shouldPrevStation(){
+    public void shouldPrevStation() {
         Radio radio = new Radio();
 
         radio.setMaxStation(9);
@@ -32,47 +32,11 @@ class RadioTest {
         radio.setNext(false);
         radio.setPrev(true);
 
-        assertEquals( false, radio.isNext(false));
-        assertEquals( true, radio.isPrev(true));
-        assertEquals( 0, radio.getNumberCurrentStation());
+        assertEquals(false, radio.isNext(false));
+        assertEquals(true, radio.isPrev(true));
+        assertEquals(0, radio.getNumberCurrentStation());
         radio.setPrevStation(9);
-        assertEquals( 9, radio.getPrevStation());
-    }
-
-    @Test
-    public void shouldIncreaseVolume(){
-        Radio radio = new Radio();
-
-        radio.setMaxVolume(10);
-        radio.setMinVolume(0);
-
-        radio.setCurrentVolume(10);
-        radio.setIncrease(true);
-        radio.setDecrease(false);
-
-        assertEquals( true, radio.isIncrease(true));
-        assertEquals( false, radio.isDecrease(false));
-        assertEquals( 10, radio.getCurrentVolume());
-        radio.setIncreaseCurrentVolume(10);
-        assertEquals( 10, radio.getIncreaseCurrentVolume());
-    }
-
-    @Test
-    public void shouldDecreaseVolume(){
-        Radio radio = new Radio();
-
-        radio.setMaxVolume(10);
-        radio.setMinVolume(0);
-
-        radio.setCurrentVolume(0);
-        radio.setIncrease(false);
-        radio.setDecrease(true);
-
-        assertEquals( false, radio.isIncrease(false));
-        assertEquals( true, radio.isDecrease(true));
-        assertEquals( 0, radio.getCurrentVolume());
-        radio.setDecreaseCurrentVolume(0);
-        assertEquals( 0, radio.getDecreaseCurrentVolume());
+        assertEquals(9, radio.getPrevStation());
     }
 
 }
